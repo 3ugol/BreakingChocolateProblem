de57717fbf8cd45b53af23a709ea3cc6c3577cdf
admin <3ugol@ukr.net>
2019-03-19 21:19:42 +0200

    https://www.codewars.com/kata/534ea96ebb17181947000ada

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n=0, m=0, k=0;
+        if (n!=0 && m!=0){
+           k=n*m-1;
+        }
+        System.out.println(k);
+    }
+}
